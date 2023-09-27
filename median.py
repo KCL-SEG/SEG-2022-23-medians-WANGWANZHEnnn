fab3a835975811c2f1972b641306c302ae645ec3
Wanzhen Wang <101357076+WANGWANZHEnnn@users.noreply.github.com>
2023-09-27 13:59:40 +0100

    Add files via upload

--- median.py
+++ median.py
@@ -1,12 +1,38 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def calculate_median(numbers):
+    numbers.sort()
+
+    if not numbers:
+       return None
+
+    middle_index = len(numbers)//2
+
+    if len(numbers) % 2 == 1:
+        return numbers[middle_index]
+    
+       
+    else:
+
+          middle1 = numbers[middle_index -1 ]
+          middle2 = numbers[middle_index]
+          return (middle1 + middle2) / 2
+
+median = None
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
+        input_str = input()
         numbers = [float(value) for value in input().split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
+        median = calculate_median(numbers)
+        if median is not None:
+            print("median:",median)
         break
-print(numbers)
+if median is not None:
+    print("Median outside the loop ! : ", median )
